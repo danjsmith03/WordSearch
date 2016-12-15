a33029be803c6f2c5677207e8275780c02d56ec0
Dan Smith <danjsmith03@gmail.com>
2016-12-15 15:32:07 -0500

    update

--- src/wordsearch/MouseMotionEvent.java
+++ src/wordsearch/MouseMotionEvent.java
@@ -10,7 +10,7 @@ import java.awt.event.*;
  * @author Daniel Smith
  * This Class is enabled when the use drags the mouse over the screen
  */
-public class MouseMotionEvent extends javax.swing.JFrame implements MouseMotionListener {
+public class MouseMotionEvent extends javax.swing.JFrame implements MouseMotionListener, MouseListener {
     
     static final String NEWLINE = System.getProperty("line.separator");
     
@@ -26,5 +26,30 @@ public class MouseMotionEvent extends javax.swing.JFrame implements MouseMotionL
     @Override
     public void mouseDragged(MouseEvent e) {
         Input.findJTextFields(e);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        Input.findJTextFields(e);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        Input.findJTextFields(e);
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        //eventOutput("Mouse released", e);
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        //eventOutput("Mouse entered", e);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        //eventOutput("Mouse exited", e);
     }
 }
